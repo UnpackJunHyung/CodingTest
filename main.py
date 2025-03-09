779dee4c1b22c0cbf4c28c8ad178ae27440b911d
ParkJunHyung <wnsgud117117@naver.com>
2025-03-09 22:32:39 +0900

    0309_2232

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('ParkJunHyung')
+    print_hi('ParkJunHyung_2232')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
